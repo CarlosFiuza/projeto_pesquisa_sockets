58e224bc4f5fa9dffe24f5a41dc217da214fa58e
carlosFiuza <carlosfiuza1234@gmail.com>
2022-04-27 19:55:39 -0300

    Comentando codigo client

--- src/client.py
+++ src/client.py
@@ -12,26 +12,30 @@ class Client:
         self.server_addr = (host, port)
         self.sel = selectors.DefaultSelector()
         self.alive = True
+        # Cria socket 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setblocking(False)
+        # Conecta com a sala com por meio de conexao não blocante
         sock.connect_ex(self.server_addr)
         print(f"Conexão bem-sucedida")
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        data = types.SimpleNamespace(connid=connid, outb=b"", recv_total=0, messages=[])
+        data = types.SimpleNamespace(connid=connid, messages=b"")
+        # registra socket para monitoramento e multiplexação de I/O
         self.sel.register(sock, events, data=data)
 
     def event_read(self):
         while True:
+            # verifica se objeto socket está pronto para I/O
             events = self.sel.select(timeout=None)
             for key, mask in events:
+                # se for evento para leitura
                 if mask & selectors.EVENT_READ:
                     sock = key.fileobj
-                    data = key.data
+                    # recebe mensagem do socket
                     recv_data = sock.recv(1024)
                     if recv_data:
                         recv_data = recv_data.decode()
                         print(f">> {recv_data[:-1]}")
-                        data.recv_total += len(recv_data)
                     else:
                         print("Desconectado da sala!")
                         self.alive = False
@@ -39,17 +43,21 @@ class Client:
 
     def event_write(self):
         while True and self.alive:
+            # verifica se objeto socket está pronto para I/O
             events = self.sel.select(timeout=None)
             for key, mask in events:
+                # se for evento para escrita
                 if mask & selectors.EVENT_WRITE:
                     sock = key.fileobj
                     data = key.data
-                    data.messages.append(sys.stdin.readline().encode())
-                    if not data.outb and data.messages:
-                        data.outb = data.messages.pop(0)
-                    if data.outb:
-                        sent = sock.send(data.outb)
-                        data.outb = data.outb[sent:]
+                    # captura entrada do cliente
+                    data.messages += sys.stdin.readline().encode()
+                    if data.messages.decode():
+                        # envia mensagem
+                        sent = sock.send(data.messages)
+                        # atualiza mensagens a serem enviadas de acordo com quantos bytes
+                        # foram enviados bem sucedidos
+                        data.messages = data.messages[sent:]
 
 
     def catch_except(self, args, /):
@@ -60,6 +68,7 @@ class Client:
     def run(self):
         try:
             print('Digite o que desejar, sendo que "listar" retorna todos os clientes na sala')
+            # cria thread para evento de leitura
             thread = threading.Thread(target = self.event_read)
             thread.start()
             # threading.excepthook = self.catch_except(self)
@@ -72,7 +81,7 @@ class Client:
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:  
-        print ("Correct usage: script, IP address, port number, connid") 
+        print ("Necessita dos argumentos: endereço IP, porta, connid") 
         exit()
     try:
         client = Client()
