38c5b4c920625f54d2b678043bea9018440e6457
carlosFiuza <carlosfiuza1234@gmail.com>
2022-04-27 19:56:10 -0300

    comentando codigo server

--- src/server.py
+++ src/server.py
@@ -8,6 +8,7 @@ class Server:
         self.room_threads = []
 
     def get_all_rooms(self):
+        # lista todas as salas do servidor
         rooms = [room.id + " " + room.addr for room in self.room_threads]
         if len(rooms) == 0:
             print("Nenhuma sala existente")
@@ -22,13 +23,17 @@ class Server:
             while True:
                 try:
                     text = input("-> ")
+                    # verifica se é comando para listar
                     if text == "listar":
                         self.get_all_rooms()
                     else:
                         host, port, num, id = text.split()
+                        # cria novo objeto Room
                         room = Room(host, int(port), int(num), id)
+                        # cria thread para nova sala
                         thread = threading.Thread(target = room.run, args = (lambda : self.server_up, ))
                         thread.start()
+                        # mapeia as salas com seus identificadores
                         room_map = types.SimpleNamespace(id=id, thread=thread, addr=host + " " + port)
                         self.room_threads.append(room_map)
                 except KeyboardInterrupt:
@@ -39,6 +44,7 @@ class Server:
         except KeyboardInterrupt:
             print("Fechando...")
             self.server_up = False
+            # aguarda encerramento das threads
             for room_map in self.room_threads:
                 room_map.thread.join()
         finally:
