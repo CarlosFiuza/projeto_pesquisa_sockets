92e1cd65544ced53af22be343cc9fb716b050169
carlosFiuza <carlosfiuza1234@gmail.com>
2022-04-26 23:20:03 -0300

    Adicionando funcionalidade de listar salas existentes no server

--- src/server.py
+++ src/server.py
@@ -1,4 +1,3 @@
-import sys
 import socket
 import selectors
 import types
@@ -109,17 +108,30 @@ class Server:
     def __init__(self):
         self.room_threads = []
 
+    def get_all_rooms(self):
+        rooms = [room.id + " " + room.addr for room in self.room_threads]
+        if len(rooms) == 0:
+            print("Nenhuma sala existente")
+        else:
+            print("Salas:")
+            print("\n".join(rooms))
+
     def run(self):
         try:
-            print("Digite o IP, porta, num_max pessoas e identificador para criar uma sala:")
+            print("-- Escreva 'listar' para receber todas as salas ativas")
+            print("-- Digite o IP, porta, num_max pessoas e identificador a qualquer momento para criar uma sala")
             while True:
                 try:
-                    host, port, num, id = input().split()
-                    room = Room(host, int(port), int(num), id)
-                    thread = threading.Thread(target = room.run, args = (lambda : self.server_up, ))
-                    thread.start()
-                    room_map = types.SimpleNamespace(id=id, thread=thread)
-                    self.room_threads.append(room_map)
+                    text = input("-> ")
+                    if text == "listar":
+                        self.get_all_rooms()
+                    else:
+                        host, port, num, id = text.split()
+                        room = Room(host, int(port), int(num), id)
+                        thread = threading.Thread(target = room.run, args = (lambda : self.server_up, ))
+                        thread.start()
+                        room_map = types.SimpleNamespace(id=id, thread=thread, addr=host + " " + port)
+                        self.room_threads.append(room_map)
                 except KeyboardInterrupt:
                     break
                 except:
